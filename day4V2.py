b2836df715dd89bbf8701755b37f836ca7708bcd
GigiMere <158825946+GigiMere@users.noreply.github.com>
2024-03-01 22:50:01 +0400

    Add files via upload

---
+++ day4V2.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+
+age = int(input("How old are you? "))
+
+future = int(input("How many years into the future do you want to time travel? "))
+
+future_age = age + future
+
+print("Hello", name + ", in", future, "years, you will be", future_age, "years old")
